880ed2cbe001700fea2a43afdbf3d2e25a673cf2
Callum Jones <callum@callumj.com>
2014-05-29 10:00:08 +0800

    Map all PKs to inserted.XXX

--- lib/active_record/connection_adapters/sqlserver/database_statements.rb
+++ lib/active_record/connection_adapters/sqlserver/database_statements.rb
@@ -298,6 +298,14 @@ module ActiveRecord
         def sql_for_insert(sql, pk, id_value, sequence_name, binds)
           sql =
             if pk
+              mapped_pks = nil
+
+              if pk.is_a?(Array)
+                mapped_pks = pk.map { |p| "inserted.#{p}" }.join(",")
+              else
+                mapped_pks = "inserted.#{pk}"
+              end
+
               sql.insert(sql.index(/ (DEFAULT )?VALUES/), " OUTPUT inserted.#{pk}")
             else
               "#{sql}; SELECT CAST(SCOPE_IDENTITY() AS bigint) AS Ident"
